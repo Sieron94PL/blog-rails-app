b4bc1a23d17366446f2af5a24090272c3829458f
Damian Sieroń <damiansieron94@gmail.com>
2018-07-12 15:38:51 +0200

    [fix] migration

--- db/migrate/20180710123242_create_authors.rb
+++ db/migrate/20180710123242_create_authors.rb
@@ -3,7 +3,7 @@ class CreateAuthors < ActiveRecord::Migration[5.2]
     create_table :authors do |t|
       t.string :name
       t.string :surname
-      t.integer :age    
+      t.integer :age
     end
   end
 end
